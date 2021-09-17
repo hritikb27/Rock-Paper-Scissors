9d83bc0ceb12db709b5e5b26f93d8426237b2a25
hritikb27 <hritikb90@gmail.com>
2021-09-17 14:02:31 +0530

    Only Valid Input Logic

--- app.js
+++ app.js
@@ -13,23 +13,27 @@ function playerSelection(){
 }
 
 const playRound = (playerSelection, computerPlay)=>{
-        if(playerSelection==moves[0] && computerPlay==moves[1]){
-            alert('Computer Wins');
+    breakMe: if(!(moves.includes(playerSelection))){
+        alert('Enter a valid option.');
+        break breakMe;}
+        
+        else if(playerSelection==moves[0] && computerPlay==moves[1]){
+            alert(`${computerPlay} Wins! ${computerPlay} beats ${playerSelection}`);
         }
         else if(playerSelection==moves[1] && computerPlay==moves[0]){
-            alert('Player 1 Wins');
+            alert(` ${playerSelection} Wins! ${playerSelection} beats ${computerPlay}`);
         }
         else if(playerSelection==moves[1]&&computerPlay==moves[2]){
-            alert('Computer Wins');
+            alert(`${computerPlay} Wins! ${computerPlay} beats ${playerSelection}`);
         }
         else if(playerSelection==moves[2]&&computerPlay==moves[1]){
-            alert('Player 1 Wins');
+            alert(`${playerSelection} Wins! ${playerSelection} beats ${computerPlay}`);
         }
         else if(playerSelection==moves[2]&&computerPlay==moves[0]){
-            alert('Computer Wins');
+            alert(`${computerPlay} Wins! ${computerPlay} beats ${playerSelection}`);
         }
         else if(playerSelection==moves[0]&&computerPlay==moves[2]){
-            alert('Player 1 Wins');
+            alert(`${playerSelection} Wins! ${playerSelection} beats ${computerPlay}`);
         }
         else{
             alert('Draw!')
