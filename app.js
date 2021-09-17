e3948ab192f050f1856ed06c224de7b38b5ff871
hritikb27 <hritikb90@gmail.com>
2021-09-17 13:46:01 +0530

    Basic Logic Added

--- app.js
+++ app.js
@@ -3,7 +3,37 @@ const moves = ['ROCK', 'PAPER', 'SCISSORS'];
 function computerPlay(){
     const random = Math.floor(Math.random()*moves.length);
 
-    console.log(moves[random]);
+    return (moves[random]);
 }
 
-computerPlay();
+function playerSelection(){
+    const playerMove = prompt('Select From: Rock, Paper, Scissors:')
+    const caseMove = playerMove.toUpperCase();
+    return caseMove;
+}
+
+const playRound = (playerSelection, computerPlay)=>{
+        if(playerSelection==moves[0] && computerPlay==moves[1]){
+            alert('Computer Wins');
+        }
+        else if(playerSelection==moves[1] && computerPlay==moves[0]){
+            alert('Player 1 Wins');
+        }
+        else if(playerSelection==moves[1]&&computerPlay==moves[2]){
+            alert('Computer Wins');
+        }
+        else if(playerSelection==moves[2]&&computerPlay==moves[1]){
+            alert('Player 1 Wins');
+        }
+        else if(playerSelection==moves[2]&&computerPlay==moves[0]){
+            alert('Computer Wins');
+        }
+        else if(playerSelection==moves[0]&&computerPlay==moves[2]){
+            alert('Player 1 Wins');
+        }
+        else{
+            alert('Draw!')
+        }
+}
+
+playRound(playerSelection(), computerPlay());
